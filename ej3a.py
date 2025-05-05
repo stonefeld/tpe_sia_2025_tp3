3cbca2f05166f80c446579043ed88bb1d78401a8
stonefeld <theo.stanfield@gmail.com>
2025-05-05 17:05:59 -0300

    Arreglo del ejercicio 3a

--- ej3a.py
+++ ej3a.py
@@ -15,11 +15,16 @@ def main():
     xor_data = [[-1, -1], [-1, 1], [1, -1], [1, 1]]
     xor_labels = [-1, 1, 1, -1]
 
-    mlp = PerceptronMulticapa(input_size=2, hidden_size=2, tita=tanh, tita_prime=tanh_prime)
+    # mlp = PerceptronMulticapa(input_size=2, hidden_size=2, tita=tanh, tita_prime=tanh_prime)
+    # mlp.train(xor_data, xor_labels)
+    # mlp = PerceptronMulticapa(capas=[35, 10, 1], tita=tanh, tita_prime=tanh_prime, alpha=0.1)
+    # mlp.train(data, labels)
+
+    mlp = PerceptronMulticapa(capas=[2, 2, 1], tita=tanh, tita_prime=tanh_prime, alpha=0.1)
     mlp.train(xor_data, xor_labels)
 
     for x in xor_data:
-        print(f"{x} => {round(mlp.predict(x))}")
+        print(f"{x} => {round(mlp.predict(x)[0])}")
 
 
 if __name__ == "__main__":
